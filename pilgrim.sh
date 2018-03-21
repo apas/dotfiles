b06483fe7790e5b08339860ceac85002fc9d3ddc
apas <apostolos.me@gmail.com>
2018-03-21 23:36:05 +0100

    improves symlink script

--- pilgrim.sh
+++ pilgrim.sh
@@ -4,13 +4,26 @@ dest=${1}
 host=${2}
 source=`pwd`
 
-echo "Install .dotfiles"
-echo " "
-echo -e "Source directory:\t"${source}
-echo -e "Destination directory:\t"${dest}
-echo -e "Hostname:\t"${host}
+install_brew() {
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        echo "== Brew"
+
+        isbrew=`which brew`
+
+        if [[ -z ${isbrew} ]]; then
+            echo "Brew not installed. Installing brew. . . "
+            /usr/bin/ruby -e "$(curl -fsSL \
+            https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
+
+        echo "Brew installed. Installing Brewfile. . . "
+        brew bundle install --file=${source}/Brewfile
+    fi
+}
 
 dotfiles() {
+    echo "== Dotfiles"
+
     ln -sf ${source}/bash_profile ${dest}/.bash_profile
     ln -sf ${source}/inputrc ${dest}/.inputrc
     ln -sf ${source}/git/gitconfig ${dest}/.gitconfig
@@ -24,6 +37,8 @@ dotfiles() {
 }
 
 directories() {
+    echo "== Directories"
+
     ln -sfn ${source}/bin ${dest}/.bin
     ln -sfn ${source}/gap ${dest}/.gap
     ln -sfn ${source}/pip ${dest}/.pip
@@ -42,19 +57,27 @@ directories() {
     rm ${source}/shell/*.bak
 }
 
-
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    brew=`which brew`
-
-    if [[ -z ${brew} ]]; then
-        echo "brew not installed. Installing brew. . . "
-        /usr/bin/ruby -e "$(curl -fsSL \
-        https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-
-    echo "brew installed. Installing Brewfile. . . "
-    brew bundle install --file=${source}/Brewfile
+if [[ ! $# -eq 2 ]]; then
+    echo "Pilgrim requires two parameters: destination path and hostname."
+    echo "Please try again."
+    echo " "
+    echo "./pilgrim.sh path/to/install hostname"
+    exit 0
 fi
 
-dotfiles
-directories
+echo "Pilgrim will symlink dotfiles from/to the following directories:"
+echo " "
+echo -e "Source directory:\t\t"${source}
+echo -e "Destination directory:\t\t"${dest}
+echo -e "Hostname to use in PS1 prompt:\t"${host}
+echo " "
+read -p "Are you sure you want to continue? (yes/no) " answer
+
+if [[ ${answer} == "yes" || ${answer} == "y" ]]; then
+    dotfiles
+    directories
+    install_brew
+else
+    echo "Exiting."
+    exit 0
+fi
