7267a2498dbe63b4022d73ed52d40fb3ea3c9609
apas <apostolos.me@gmail.com>
2018-07-01 11:31:54 +0200

    changes shell to brew bash during installation

--- pilgrim.sh
+++ pilgrim.sh
@@ -11,13 +11,16 @@ install_brew() {
         isbrew=$(which brew)
 
         if [[ -z ${isbrew} ]]; then
-            echo "Brew not installed. Installing brew. . . "
+            echo "Brew not installed. Installing brew. . ."
             /usr/bin/ruby -e "$(curl -fsSL \
             https://raw.githubusercontent.com/Homebrew/install/master/install)"
         fi
 
-        echo "Brew installed. Installing Brewfile. . . "
+        echo "Brew installed. Installing Brewfile. . ."
         brew bundle install --file=${source}/Brewfile
+
+        echo "Changing shell to brew's bash. . ."
+        chsh -s $(brew --prefix)/bin/bash
     fi
 }
 
