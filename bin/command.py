9fc2e5fc460c49b5e3ccb5598d81f9feec8aab87
apas <apostolos.me@gmail.com>
2022-02-16 14:07:12 +0200

    refactors slapdash tz conversion command

--- bin/command.py
+++ bin/command.py
@@ -14,64 +14,63 @@ tz_dict = {
     "sf": "America/Los_Angeles"
 }
 
+def print_prompt(title, value, exit):
+    print(json.dumps({
+        "view": {
+            "type": "list",
+            "options": [
+                {
+                    "title": title,
+                    "action": {
+                    "type": "copy",
+                    "value": value
+                    }
+                }
+            ]
+        }
+    }))
+
+    if exit:
+        sys.exit()
+
 if len(sys.argv) > 1:
     user_input = sys.argv[1]
 
     # input_string = "1030pm sf ath"
     # slapdash appends a --query= prefix in the arguments string when rerunning a Form
     input_string = user_input.replace("--query=", "", 1)
-    input_time = input_string.split()[0]
     try:
+        input_time = input_string.split()[0]
         input_tz_from = input_string.split()[1]
         input_tz_to = input_string.split()[2]
     except IndexError:
-        sys.exit()
-    has_minutes = False
+        print_prompt("Wrong input, please try again.", "Wrong input, please try again.", True)
 
-    try:
-        input_time_dissect = time.strptime(input_time, "%I%p")
-    except ValueError:
-        input_time_dissect = time.strptime(input_time, "%I%M%p")
-        has_minutes = True
+    if (input_tz_from in tz_dict.keys()) and (input_tz_to in tz_dict.keys()):
+        try:
+            input_time_dissect = time.strptime(input_time, "%I%p")
+        except ValueError:
+            input_time_dissect = time.strptime(input_time, "%I%M%p")
 
-    input_hour = input_time_dissect.tm_hour
-    input_min = input_time_dissect.tm_min
+        input_hour = input_time_dissect.tm_hour
+        input_min = input_time_dissect.tm_min
 
-    today = datetime.date.today()
-    day = today.strftime("%d")
-    month = today.strftime("%m")
-    year = today.strftime("%Y")
+        today = datetime.date.today()
+        day = today.strftime("%d")
+        month = today.strftime("%m")
+        year = today.strftime("%Y")
 
-    if input_tz_from in tz_dict.keys():
-        if not has_minutes:
-            # input_time_object = datetime.datetime.strptime(input_time, "%I%p").time()
-            input_time_object = datetime.datetime(int(year), int(month), int(day), input_hour,
-                tzinfo=zoneinfo.ZoneInfo(key=tz_dict[input_tz_from]))
-        else:
-            input_time_object = datetime.datetime(int(year), int(month), int(day), input_hour, input_min,
-                tzinfo=zoneinfo.ZoneInfo(key=tz_dict[input_tz_from]))
+        input_time_object = datetime.datetime(int(year), int(month), int(day),
+            input_hour, input_min, tzinfo=zoneinfo.ZoneInfo(key=tz_dict[input_tz_from]))
 
-    # as_tz = input_time_object.astimezone(pytz.timezone(tz_list[1]))
-    if input_tz_to in tz_dict.keys():
         as_tz = input_time_object.astimezone(pytz.timezone(tz_dict[input_tz_to]))
 
-    res = as_tz.strftime("%H") + ":" + as_tz.strftime("%M") + as_tz.strftime("%p")
+        res = as_tz.strftime("%H") + ":" + as_tz.strftime("%M") + " " + as_tz.strftime("%p") + " " + input_tz_to.upper()
 
-    print(json.dumps({
-        "view": {
-              "type": "list",
-              "options": [
-                  {
-                      "title": str(res),
-                      "action": {
-                          "type": "copy",
-                          "value": str(res)
-                      }
-                  }
-              ]
-              }
-        }))
-    sys.exit()
+        print_prompt(res, res, True)
+    else:
+        print_prompt("Wrong input, please try again.", "Wrong input, please try again.", True)
+
 
 print(json.dumps({
     "view": {
